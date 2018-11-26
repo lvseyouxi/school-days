a1971e3427ecf01ceae9628c67c539bf3be08280
hahaha <864500887@qq.com>
2018-11-26 19:57:37 +0800

    123

---
+++ New Unity Project/Assets/changescene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class changescene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void OnClick()
+    {
+        SceneManager.LoadScene("ohok");
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ New Unity Project/Assets/xinyouxi.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class xinyouxi : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OnGUI()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
