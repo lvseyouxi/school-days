107256080ac7c531bdee685c48e34654d97f8b03
hahaha <864500887@qq.com>
2018-11-28 20:07:25 +0800

    123

--- School 1.0test/Assets/Scripts/UI Controller/getnumber.cs
+++ School 1.0test/Assets/Scripts/UI Controller/getnumber.cs
@@ -9,15 +9,15 @@ public class getnumber: MonoBehaviour {
     Text money,reputation,efficence,teach,manage,science;
     // Use this for initialization
     public SchoolNumerical now1;//取值
+    
 	void Start () {
-        int x1 = 0;
 
-        money.text= "经费："+x1+"k";
-        reputation.text = "声望：";
-        efficence.text = "效率：";
-        teach.text = "教学：";
-        manage.text = "管理：" ;
-        science.text = "科研：" ;
+        money.text= "经费："+now1.money+"k";
+        reputation.text = "声望："+ now1.prestige;
+        efficence.text = "效率：" + now1.morale;
+        teach.text = "教学：" + now1.education;
+        manage.text = "管理：" + now1.management;
+        science.text = "科研：" + now1.research;
     }
 	
 	// Update is called once per frame
